1bb55a38304e4a4d95eb841ebcdd377a4516d4b7
mattnj58 <mattnj58@gmail.com>
2019-01-18 16:26:24 -0500

    added 3 buttons for social

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './contact.css';
-import gif from './giphy.gif';
 
 class Contact extends Component{
 
@@ -9,7 +8,9 @@ class Contact extends Component{
             <div className='contact-section'>
                 <div className='padding-top'></div>
                 <h2>Contact Information</h2>
-                <h1 class="section-subtitle"> Thanks for gracing me with your presence! If you have any questions about me, feel free to contact me at <a href='mailto:mattnj58@gmail.com'>mattnj58@gmail.com</a> </h1>
+                <h1 class="section-subtitle">
+                    Thanks for visiting! If you have any questions about me, feel free to contact me at <a href='mailto:mattnj58@gmail.com'>mattnj58@gmail.com</a>
+                </h1>
                 <div class="padding--bottom"></div>
             </div>
         );
--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './welcome.css';
 import profile from './profile.jpg'
-
+import Button from '@material-ui/core/Button'
 
 class Welcome extends Component{
     render() {
@@ -16,11 +16,34 @@ class Welcome extends Component{
                 </div>
                 <div className='aboutMe--section'>
                     Hello! Welcome to my site! I'm Matthew Wong, a senior at Lehigh University
+                    <div class="row">
+                        <div className='center'>
+                            <a href='https://github.com/mattnj58'>
+                                <Button variant='extendedFab' color='primary'>
+                                    Github
+                                </Button>
+                            </a>
+                            <h class="margin"></h>
+                            <a href='https://www.linkedin.com/in/matt-wong-01401652/'>
+                                <Button variant='extendedFab' color='primary'>
+                                    LinkedIn
+                                </Button>
+                            </a>
+                            <div></div>
+                            <a href='/Resume.pdf' class="view-pdf" target="./Resume.pdf">
+                                <Button variant='extendedFab' color='primary'>
+                                    Resume
+                                </Button>
+                            </a>
+                        </div>
+                    </div>
                     <div className='padding--bottom'></div>
                 </div>
             </div>
         )
     }
 }
+
+
 
 export default Welcome;
