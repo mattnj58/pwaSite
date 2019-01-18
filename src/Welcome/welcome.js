c8ec2864c943960e831216def11779b6f099de0a
mattnj58 <mattnj58@gmail.com>
2019-01-18 17:05:57 -0500

    added 3 buttons for social

--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import './welcome.css';
 import profile from './profile.jpg'
 import Button from '@material-ui/core/Button'
+import {Document, Page} from 'react-pdf';
 
 class Welcome extends Component{
+
+    state = { numPages: null, pageNumber: 1 };
+
+    onDocumentLoadSuccess = ({ numPages }) => {
+        this.setState({ numPages });
+    };
+
+    goToPrevPage = () =>
+        this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+    goToNextPage = () =>
+        this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+
+
     render() {
+        const {pageNumber, numPages} = this.state;
+
         return(
             <div className='welcome'>
                 <div className='profile--section'>
@@ -18,19 +34,19 @@ class Welcome extends Component{
                     Hello! Welcome to my site! I'm Matthew Wong, a senior at Lehigh University
                     <div class="row">
                         <div className='center'>
-                            <a href='https://github.com/mattnj58'>
+                            <a href='https://github.com/mattnj58' target="_blank" rel='noopener'>
                                 <Button variant='extendedFab' color='primary'>
                                     Github
                                 </Button>
                             </a>
                             <h class="margin"></h>
-                            <a href='https://www.linkedin.com/in/matt-wong-01401652/'>
+                            <a href='https://www.linkedin.com/in/matt-wong-01401652/' target='_blank' rel='noopener'>
                                 <Button variant='extendedFab' color='primary'>
                                     LinkedIn
                                 </Button>
                             </a>
                             <div></div>
-                            <a href='/Resume.pdf' class="view-pdf" target="_blank">
+                            <a href={process.env.PUBLIC_URL+'/resume.pdf'} target="_blank">
                                 <Button variant='extendedFab' color='primary'>
                                     Resume
                                 </Button>
