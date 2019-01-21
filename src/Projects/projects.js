7901e23f92ca46424fd2fcecbfa05a8868a99f33
mattnj58 <mattnj58@gmail.com>
2019-01-21 14:24:58 -0500

    removed the bullet points due to css issues

--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -29,24 +29,27 @@ class Projects extends Component {
                 <h1 className='title'>Projects</h1>
                 <h3 className='project'> Wall Monitor Control Application</h3>
                     <ul>
-                        <li>Worked as part of a 3-person team to design, implement, and test an application to control video sources across several dozen displays</li>
-                        <li>Technical Skill: Python, Angular 2, Postgres, TypeScript, Barco CMS </li>
+                        Worked as part of a 3-person team to design, implement, and test an application to control video sources across several dozen displays
+                        <div></div>
+                        Technical Skill: Python, Angular 2, Postgres, TypeScript, Barco CMS
                     </ul>
                 <h3 className='project'> Social Media Application </h3>
                 <ul>
-                    <li>Created a multi-component program that stored users in a database and allowed them to communicate using their phone or pc </li>
-                    <li>Technical Skills: Typescript, Maven, Docker, Android Studio, Java, Bootstrap </li>
+                    Created a multi-component program that stored users in a database and allowed them to communicate using their phone or pc
+                    <div></div>
+                    Technical Skills: Typescript, Maven, Docker, Android Studio, Java, Bootstrap
                 </ul>
                 <h3 className='project'> Personal Website</h3>
                 <ul>
-                    <li> Developed a website using modern design and technology </li>
-                    <li>Technical Skills: JavaScript, CSS, HTML, Bootstrap, Github</li>
-
+                    Developed a website using modern design and technology
+                    <div></div>
+                    Technical Skills: JavaScript, CSS, HTML, Bootstrap, Github
                 </ul>
                 <h3 className='project'> Car Rental System </h3>
                 <ul>
-                    <li>Designed and implemented a car rental system that allowed users to login, view what cars were available, the prices, and rent a car for a user determined amount of time </li>
-                    <li>Technical Skills: Java, SQL, Oracle</li>
+                    Designed and implemented a car rental system that allowed users to login, view what cars were available, the prices, and rent a car for a user determined amount of time
+                    <div></div>
+                    Technical Skills: Java, SQL, Oracle
                 </ul>
                 <div className='padding--bottom'></div>
             </div>
